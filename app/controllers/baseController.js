8a4053abeebaa8f04993c03c61efccdc7fc56be5
alehoppai <aleh_belski@outlook.com>
2024-10-06 15:38:48 +0200

    fix -- removing unused console.log

--- app/controllers/baseController.js
+++ app/controllers/baseController.js
@@ -12,7 +12,6 @@ export class BaseController {
    */
   constructor(routes) {
     for (const [method, path, handler] of routes) {
-      console.log(method, path, handler)
       this.router[method.trim().toLowerCase()](path, this[handler].bind(this))
     }
   }
